bf37acd83c8cde69668e92a54ab7ab673dd48bc0
AssylbekDanyshbek <danyshbek1996@gmail.com>
2023-11-28 17:52:23 +0600

    Added jquery sample

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,94 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  $('#gridContainer').dxDataGrid({
+    dataSource: generateData(100),
+    keyExpr: 'id',
+    showBorders: true,
+    columnWidth: 100,
+    selection: {
+      mode: "multiple"
+    },
+    columns: [
+      {
+        dataField: "firstName",
+        fixed: true,
+        width: 80,
+        fixedPosition: "left"
+      },
+      'lastName',
+      'supervisor',
+      'startedDate',
+      'position',
+      {
+        dataField: "gender",
+        fixed: true,
+        width: 80,
+        fixedPosition: "right"
+      },
+      'birthDate'
+    ],
+    loadPanel: {
+      enabled: true,
+    },
+    scrolling: {
+      mode: 'virtual',
+    },
+    sorting: {
+      mode: 'none',
+    },
+    onContentReady(e) {
+      e.component.option('loadPanel.enabled', false);
     },
   });
 });
+
+let s = 123456789;
+const random = function () {
+  s = (1103515245 * s + 12345) % 2147483647;
+  return s % (10 - 1);
+};
+
+const posRandom = function () {
+  s = (1103515245 * s + 12345) % 2147483647;
+  return s % (4 - 1);
+};
+
+
+const generateData = function (count) {
+  let i;
+  const surnames = ['Smith', 'Johnson', 'Brown', 'Taylor', 'Anderson', 'Harris', 'Clark', 'Allen', 'Scott', 'Carter'];
+  const names = ['James', 'John', 'Robert', 'Christopher', 'George', 'Mary', 'Nancy', 'Sandra', 'Michelle', 'Betty'];
+  const gender = ['Male', 'Female'];
+  const positions = ["Trainee", "Junior", "Middle", "Senior"];
+  const items = [];
+  const startBirthDate = Date.parse('1/1/1975');
+  const endBirthDate = Date.parse('1/1/1992');
+  
+  const empStartDate = Date.parse('1/1/2018');
+  const empEndDate = Date.now();
+
+  for (i = 0; i < count; i += 1) {
+    const birthDate = new Date(startBirthDate + Math.floor(
+      (random() * (endBirthDate - startBirthDate)) / 10,
+    ));
+    birthDate.setHours(12);
+    
+    const emplDate = new Date(empStartDate + Math.floor(
+      (random() * (empEndDate - empStartDate)) / 10,
+    ));
+    emplDate.setHours(12);
+
+    const nameIndex = random();
+    const item = {
+      id: i + 1,
+      firstName: names[nameIndex],
+      lastName: surnames[random()],
+      supervisor: names[nameIndex],
+      position: positions[posRandom()],
+      gender: gender[Math.floor(nameIndex / 5)],
+      birthDate,
+      startedDate: emplDate
+    };
+    items.push(item);
+  }
+  return items;
+};
