7496da659d250f3fbadccc34c018b06f2da13753
AssylbekDanyshbek <danyshbek1996@gmail.com>
2023-12-07 17:44:56 +0600

    Fixed angular lint warnings

--- Angular/src/app/app.service.ts
+++ Angular/src/app/app.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
 
-export class Employee {
-  id!: number;
-
-  firstName!: string;
-
-  lastName!: string;
-
-  gender!: string;
-
-  birthDate!: Date;
+export interface Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  birthDate: Date;
+  supervisor: string;
+  position: string;
+  startedDate: Date;
 }
 
 const employees: Employee[] = [];
@@ -40,46 +39,45 @@ const names: string[] = [
 
 const gender: string[] = ['Male', 'Female'];
 
-const positions: string[] = ["Trainee", "Junior", "Middle", "Senior"];
+const positions: string[] = ['Trainee', 'Junior', 'Middle', 'Senior'];
 
 let s = 123456789;
 
 @Injectable()
 export class Service {
-  random() {
+  random(): number {
     s = (1103515245 * s + 12345) % 2147483647;
     return s % (10 - 1);
   }
 
-  randomName() {
+  randomName(): number {
     s = (2103515245 * s + 12345) % 2147483647;
     return s % (10 - 1);
   }
 
-  posRandom() {
+  posRandom(): number {
     s = (1103515245 * s + 12345) % 2147483647;
     return s % (4 - 1);
-  };
+  }
 
-  generateData(count: number) {
-    let i: number;
+  generateData(count: number): Employee[] {
     const startBirthDate = Date.parse('1/1/1975');
     const endBirthDate = Date.parse('1/1/1992');
 
     const empStartDate = Date.parse('1/1/2018');
     const empEndDate = Date.now();
 
-    for (i = 0; i < count; i++) {
+    for (let i = 0; i < count; i++) {
       const birthDate = new Date(startBirthDate + Math.floor(
         this.random()
                     * (endBirthDate - startBirthDate) / 10,
       ));
       birthDate.setHours(12);
 
-      const emplDate = new Date(empStartDate + Math.floor(
+      const employmentDate = new Date(empStartDate + Math.floor(
         (this.random() * (empEndDate - empStartDate)) / 10,
       ));
-      emplDate.setHours(12);
+      employmentDate.setHours(12);
 
       const nameIndex = this.random();
       const supIndex = this.randomName();
@@ -91,7 +89,7 @@ export class Service {
         birthDate,
         supervisor: names[supIndex],
         position: positions[this.posRandom()],
-        startedDate: emplDate
+        startedDate: employmentDate,
       };
       employees.push(item);
     }
